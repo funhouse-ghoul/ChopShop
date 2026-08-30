67d833ed36af93cc500f223f80a2e23aba58d320
aluminumbulb <funhouseghoul@gmail.com>
2026-08-29 20:06:07 -0700

    Made player a CharcterBody3d, beginning work on 1d linear motion

--- Scripts/2d_player_mov.gd
+++ Scripts/2d_player_mov.gd
@@ -1,19 +1,31 @@
-extends MeshInstance3D
+extends CharacterBody3D
 
 @export var y_rot_spd := 1.0  
+@export var x_lin_spd := 1.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("io+r"):
-		process_rotation(delta, -y_rot_spd)
-	elif Input.is_action_pressed("io-r"):
-		process_rotation(delta, y_rot_spd)
-	pass
-
-func process_rotation(delta, mag = y_rot_spd):
-	transform.basis = transform.basis.rotated(transform.basis.y, mag*delta)
+	#Rotation logic
+	process_rotation(delta)
 	
+	#Linear Movement Logic, 1 dimension
+	if Input.is_action_pressed("io+x"):
+		process_lin_move(delta, x_lin_spd)
+	elif Input.is_action_pressed("io-x"):
+		process_lin_move(delta, -x_lin_spd)
+	
+	
+
+func process_rotation(delta):
+	if Input.is_action_pressed("io+r"):
+		transform.basis = transform.basis.rotated(transform.basis.y, -y_rot_spd*delta)
+	elif Input.is_action_pressed("io-r"):
+		transform.basis = transform.basis.rotated(transform.basis.y, y_rot_spd*delta)
+	
+
+func process_lin_move(delta, mag):
+	move_and_slide()
