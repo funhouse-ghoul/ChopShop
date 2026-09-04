ace933cc568d736a3e31d6513a5676d39e3ba33b
aluminumbulb <funhouseghoul@gmail.com>
2026-09-04 13:31:30 -0700

    Ran into bug in understanding of coordinate system, fixing

--- Scripts/1d_player_mov.gd
+++ Scripts/1d_player_mov.gd
@@ -5,7 +5,6 @@ extends PathFollow3D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("io+x"):
--- Scripts/2d_player_mov.gd
+++ Scripts/2d_player_mov.gd
@@ -3,7 +3,8 @@ extends CharacterBody3D
 @export var y_rot_spd := 1.0  
 @export var x_lin_spd := 1.0
 @export var y_lin_spd := 1.0
-
+enum Move_Style {TANK, FLAT2D}
+@export var mov_sty := Move_Style.TANK
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,8 +12,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	#---Movement---
-	lin_1d_and_rot(delta)
-	#lin_2d_move()
+	match mov_sty:
+		Move_Style.TANK:
+			lin_1d_and_rot(delta)
+		Move_Style.FLAT2D:
+			lin_2d_move()
+
 	move_and_slide()
 	#---End---
 
@@ -34,8 +39,8 @@ func lin_2d_move():
 
 #Applies motion to object in x axis
 func proc_lin_move_x():
-	velocity.x = Input.get_axis("io+x","io-x")*-x_lin_spd
+	velocity += basis.x*Input.get_axis("io+x","io-x")*-x_lin_spd
 
 #Applies motion to object in y axis
 func proc_lin_move_y():
-	velocity.y = Input.get_axis("io+y","io-y")*y_lin_spd
+	velocity += basis.z*Input.get_axis("io+y","io-y")*y_lin_spd
