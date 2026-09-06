d7f59a4818424075c7b567371393b93edc5846d1
aluminumbulb <funhouseghoul@gmail.com>
2026-09-05 22:03:08 -0700

    Finished multi-target

--- Scripts/PlayerView.gd
+++ Scripts/PlayerView.gd
@@ -10,7 +10,6 @@ enum Cam_Style {PLAYERFOLLOW, PLAYERFACING, MULTIFOCUS}
 func _ready() -> void:
 	assert(player)
 	update_enemies_list()
-	print(enemies)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,6 +18,8 @@ func _process(delta: float) -> void:
 			player_only_follow()
 		Cam_Style.PLAYERFACING:
 			face_player_forward()
+		Cam_Style.MULTIFOCUS:
+			multifocus()
 	pass
 
 func cam_focus(new_loc: Vector3):
@@ -33,3 +34,13 @@ func face_player_forward():
 	
 func update_enemies_list():
 	enemies = get_tree().get_nodes_in_group("enemies")
+
+#Relies on enemies list
+#Points camera ad middle distance between several focus points and player
+func multifocus():
+	var rel_en_pos = player.position
+	for en in enemies:
+		rel_en_pos += en.position
+	rel_en_pos = rel_en_pos/(enemies.size()+1)
+	
+	cam_focus(rel_en_pos)
